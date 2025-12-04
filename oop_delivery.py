abb271a17e0361c565c745b473cb0f15ff3d8bf0
Hugh Jaiden <hugh.j@ku.th>
2025-12-04 15:55:58 +0700

    Cleanup

--- oop_delivery.py
+++ oop_delivery.py
@@ -1,16 +1,16 @@
 class Person:
     def __init__(self, name):
         self.__name = name
-    
-    def introduce(self):
-        print(f"Hi, I'm {self.__name}.")
-    
+
     @property
     def name(self):
         return self.__name
-    
+
     def __str__(self):
         return self.name
+
+    def introduce(self):
+        print(f"Hi, I'm {self.__name}.")
 
 
 class Customer(Person):
@@ -18,7 +18,14 @@ class Customer(Person):
         super().__init__(name)
         self.__address = address
         self.__orders = []
-    
+
+    @property
+    def orders(self):
+        return self.__orders    
+    @property
+    def address(self):
+        return self.__address
+
     def place_order(self, item):
         order = DeliveryOrder(self, item)
         self.__orders.append(order)
@@ -32,15 +39,6 @@ class Customer(Person):
             print("Order not found.")
             return False
 
-    @property
-    def orders(self):
-        return self.__orders    
-    
-    @property
-    def address(self):
-        return self.__address
-
-
 
 class DeliveryOrder:
     def __init__(self, customer: Customer, item):
@@ -52,14 +50,12 @@ class DeliveryOrder:
     @property
     def item(self):
         return self.__item
-    
     @property
     def status(self):
         return self.__status
     @status.setter
     def status(self, new_status):
         self.__status = new_status
-    
     @property
     def driver(self):
         return self.__driver
@@ -81,21 +77,9 @@ class Driver(Person):
         self.__vehicle = vehicle
         self.__orders = []
     
-    
-    def deliver(self, order: DeliveryOrder):
-        if order.driver != self:
-            print("The order is assigned to another driver.")
-            return
-        if not order.customer.remove_order(order) or not self.remove_order(order):
-            return
-
-        print(f"{self.name} is delivering {order.item} to {order.customer} using {self.vehicle}.")
-        order.status = "delivered"
-    
     @property
     def vehicle(self):
         return self.__vehicle
-    
     @property
     def orders(self):
         return self.__orders
@@ -110,6 +94,18 @@ class Driver(Person):
         except ValueError:
             print("Order not found.")
             return False
+
+    def deliver(self, order: DeliveryOrder):
+        if order.driver != self:
+            print("The order is assigned to another driver.")
+            return
+        if not order.customer.remove_order(order) or not self.remove_order(order):
+            return
+
+        print(f"{self.name} is delivering {order.item} to {order.customer} using {self.vehicle}.")
+        order.status = "delivered"
+    
+
 
 if __name__ == "__main__":
     alice = Customer("Alice", "A")
