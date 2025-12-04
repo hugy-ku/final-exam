9fad41dde7927b1cd504a187534af022ffd49e3f
Hugh Jaiden <hugh.j@ku.th>
2025-12-04 15:29:18 +0700

    Change attributes to private

--- oop_delivery.py
+++ oop_delivery.py
@@ -1,44 +1,75 @@
 class Person:
     def __init__(self, name):
-        self.name = name
+        self.__name = name
     
     def introduce(self):
-        print(f"Hi, I'm {self.name}.")
+        print(f"Hi, I'm {self.__name}.")
+    
+    @property
+    def name(self):
+        return self.__name
+    
+    def __str__(self):
+        return self.name
 
 
 class Customer(Person):
     def __init__(self, name, address):
         super().__init__(name)
-        self.address = address
+        self.__address = address
     
     def place_order(self, item):
         return DeliveryOrder(self, item)
+    
+    @property
+    def address(self):
+        return self.__address
+
 
 
 class DeliveryOrder:
     def __init__(self, customer: Customer, item):
         self.customer = customer
-        self.item = item
-        self.status = "preparing"
-        self.driver = "None"
+        self.__item = item
+        self.__status = "preparing"
+        self.__driver = "None"
+
+    @property
+    def item(self):
+        return self.__item
+    
+    @property
+    def status(self):
+        return self.__status
+    @status.setter
+    def status(self, new_status):
+        self.__status = new_status
+    
+    @property
+    def driver(self):
+        return self.__driver
 
     def assign_driver(self, driver):
-        self.driver = driver
+        self.__driver = driver
     
     def summary(self):
         print("Order Summary:")
-        print(f"Item: {self.item}")
-        print(f"Status: {self.status}")
-        print(f"Driver: {self.driver.name}")
+        print(f"Item: {self.__item}")
+        print(f"Status: {self.__status}")
+        print(f"Driver: {self.__driver}")
 
 
 class Driver(Person):
     def __init__(self, name, vehicle):
         super().__init__(name)
-        self.vehicle = vehicle
+        self.__vehicle = vehicle
+    
+    @property
+    def vehicle(self):
+        return self.__vehicle
     
     def deliver(self, order: DeliveryOrder):
-        print(f"{self.name} is delivering {order.item} to {order.customer.name} using {self.vehicle}.")
+        print(f"{self.name} is delivering {order.item} to {order.customer} using {self.vehicle}.")
         order.status = "delivered"
 
 if __name__ == "__main__":
