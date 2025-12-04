897a1e7e5ddc14462c7439e226349d406d395807
Hugh Jaiden <hugh.j@ku.th>
2025-12-04 14:56:34 +0700

    Add initial Customer class

--- oop_delivery.py
+++ oop_delivery.py
@@ -4,3 +4,15 @@ class Person:
     
     def introduce(self):
         print(f"Hi, I'm {self.name}")
+
+class Customer(Person):
+    def __init__(self, name, address):
+        super().__init__(name)
+        self.address = address
+    
+    def place_order(self, item):
+        # return DeliveryOrder
+        pass
+
+class DeliveryOrder:
+    pass
